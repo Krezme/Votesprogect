217c3b589e45f03792b71fb2bc57bdf0a78bc4d5
Georgi Aleksandrov (Krezme) <karakonjo5818@gmail.com>
2019-10-01 00:49:26 +0300

    VotesProgect
    The project is finished apart of Evaluation which needs an update and Test Plan which needs to be finished.

---
+++ VotesCode/Politics/Form1.Designer.cs
@@ -0,0 +1,210 @@
+﻿namespace Politics
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.candidate1label = new System.Windows.Forms.Label();
+            this.candidate2label = new System.Windows.Forms.Label();
+            this.candidate3label = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.canNum1 = new System.Windows.Forms.NumericUpDown();
+            this.canNum2 = new System.Windows.Forms.NumericUpDown();
+            this.canNum3 = new System.Windows.Forms.NumericUpDown();
+            this.canNum4 = new System.Windows.Forms.NumericUpDown();
+            this.candidate4label = new System.Windows.Forms.Label();
+            this.submit = new System.Windows.Forms.Button();
+            this.resultTXT1 = new System.Windows.Forms.TextBox();
+            this.resultTXT2 = new System.Windows.Forms.TextBox();
+            this.resultTXT3 = new System.Windows.Forms.TextBox();
+            this.resultTXT4 = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.canNum1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.canNum2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.canNum3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.canNum4)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // candidate1label
+            // 
+            this.candidate1label.AutoSize = true;
+            this.candidate1label.Location = new System.Drawing.Point(116, 77);
+            this.candidate1label.Name = "candidate1label";
+            this.candidate1label.Size = new System.Drawing.Size(39, 17);
+            this.candidate1label.TabIndex = 3;
+            this.candidate1label.Text = "can1";
+            // 
+            // candidate2label
+            // 
+            this.candidate2label.AutoSize = true;
+            this.candidate2label.Location = new System.Drawing.Point(114, 122);
+            this.candidate2label.Name = "candidate2label";
+            this.candidate2label.Size = new System.Drawing.Size(39, 17);
+            this.candidate2label.TabIndex = 4;
+            this.candidate2label.Text = "can2";
+            // 
+            // candidate3label
+            // 
+            this.candidate3label.AutoSize = true;
+            this.candidate3label.Location = new System.Drawing.Point(114, 172);
+            this.candidate3label.Name = "candidate3label";
+            this.candidate3label.Size = new System.Drawing.Size(39, 17);
+            this.candidate3label.TabIndex = 5;
+            this.candidate3label.Text = "can3";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(364, 137);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(129, 52);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Submit Box";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // canNum1
+            // 
+            this.canNum1.Location = new System.Drawing.Point(209, 77);
+            this.canNum1.Name = "canNum1";
+            this.canNum1.Size = new System.Drawing.Size(120, 22);
+            this.canNum1.TabIndex = 7;
+            // 
+            // canNum2
+            // 
+            this.canNum2.Location = new System.Drawing.Point(209, 122);
+            this.canNum2.Name = "canNum2";
+            this.canNum2.Size = new System.Drawing.Size(120, 22);
+            this.canNum2.TabIndex = 8;
+            // 
+            // canNum3
+            // 
+            this.canNum3.Location = new System.Drawing.Point(209, 170);
+            this.canNum3.Name = "canNum3";
+            this.canNum3.Size = new System.Drawing.Size(120, 22);
+            this.canNum3.TabIndex = 9;
+            // 
+            // canNum4
+            // 
+            this.canNum4.Location = new System.Drawing.Point(209, 214);
+            this.canNum4.Name = "canNum4";
+            this.canNum4.Size = new System.Drawing.Size(120, 22);
+            this.canNum4.TabIndex = 11;
+            // 
+            // candidate4label
+            // 
+            this.candidate4label.AutoSize = true;
+            this.candidate4label.Location = new System.Drawing.Point(114, 216);
+            this.candidate4label.Name = "candidate4label";
+            this.candidate4label.Size = new System.Drawing.Size(39, 17);
+            this.candidate4label.TabIndex = 10;
+            this.candidate4label.Text = "can4";
+            // 
+            // submit
+            // 
+            this.submit.Location = new System.Drawing.Point(200, 268);
+            this.submit.Name = "submit";
+            this.submit.Size = new System.Drawing.Size(129, 52);
+            this.submit.TabIndex = 12;
+            this.submit.Text = "Finish";
+            this.submit.UseVisualStyleBackColor = true;
+            this.submit.Click += new System.EventHandler(this.Submit_Click);
+            // 
+            // resultTXT1
+            // 
+            this.resultTXT1.Location = new System.Drawing.Point(166, 338);
+            this.resultTXT1.Name = "resultTXT1";
+            this.resultTXT1.Size = new System.Drawing.Size(185, 22);
+            this.resultTXT1.TabIndex = 13;
+            // 
+            // resultTXT2
+            // 
+            this.resultTXT2.Location = new System.Drawing.Point(166, 366);
+            this.resultTXT2.Name = "resultTXT2";
+            this.resultTXT2.Size = new System.Drawing.Size(185, 22);
+            this.resultTXT2.TabIndex = 14;
+            // 
+            // resultTXT3
+            // 
+            this.resultTXT3.Location = new System.Drawing.Point(166, 394);
+            this.resultTXT3.Name = "resultTXT3";
+            this.resultTXT3.Size = new System.Drawing.Size(185, 22);
+            this.resultTXT3.TabIndex = 15;
+            // 
+            // resultTXT4
+            // 
+            this.resultTXT4.Location = new System.Drawing.Point(166, 422);
+            this.resultTXT4.Name = "resultTXT4";
+            this.resultTXT4.Size = new System.Drawing.Size(185, 22);
+            this.resultTXT4.TabIndex = 16;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(517, 450);
+            this.Controls.Add(this.resultTXT4);
+            this.Controls.Add(this.resultTXT3);
+            this.Controls.Add(this.resultTXT2);
+            this.Controls.Add(this.resultTXT1);
+            this.Controls.Add(this.submit);
+            this.Controls.Add(this.canNum4);
+            this.Controls.Add(this.candidate4label);
+            this.Controls.Add(this.canNum3);
+            this.Controls.Add(this.canNum2);
+            this.Controls.Add(this.canNum1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.candidate3label);
+            this.Controls.Add(this.candidate2label);
+            this.Controls.Add(this.candidate1label);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.canNum1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.canNum2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.canNum3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.canNum4)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label candidate1label;
+        private System.Windows.Forms.Label candidate2label;
+        private System.Windows.Forms.Label candidate3label;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.NumericUpDown canNum1;
+        private System.Windows.Forms.NumericUpDown canNum2;
+        private System.Windows.Forms.NumericUpDown canNum3;
+        private System.Windows.Forms.NumericUpDown canNum4;
+        private System.Windows.Forms.Label candidate4label;
+        private System.Windows.Forms.Button submit;
+        private System.Windows.Forms.TextBox resultTXT1;
+        private System.Windows.Forms.TextBox resultTXT2;
+        private System.Windows.Forms.TextBox resultTXT3;
+        private System.Windows.Forms.TextBox resultTXT4;
+    }
+}
+
---
+++ VotesCode/Politics/Form1.cs
@@ -0,0 +1,175 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Politics
+{
+    public partial class Form1 : Form
+    {
+        int a;
+        int b;
+        int c;
+        int d;
+        int boxSummaryA;
+        int boxSummaryB;
+        int boxSummaryC;
+        int boxSummaryD;
+        int biastResults;
+        int[] resultSort = new int[4];
+        int canAPlacement;
+        int canBPlacement;
+        int canCPlacement;
+        int canDPlacement;
+
+        string canNameA = "can1";
+        string canNameB = "can2";
+        string canNameC = "can3";
+        string canNameD = "can4";
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+
+            a = Convert.ToInt32(Math.Round(canNum1.Value, 0));
+            b = Convert.ToInt32(Math.Round(canNum2.Value, 0));
+            c = Convert.ToInt32(Math.Round(canNum3.Value, 0));
+            d = Convert.ToInt32(Math.Round(canNum4.Value, 0));
+
+            boxSummaryA = boxSummaryA + a;
+            boxSummaryB = boxSummaryB + b;
+            boxSummaryC = boxSummaryC + c;
+            boxSummaryD = boxSummaryD + d;
+
+            canNum1.TabIndex = 0;
+            canNum2.TabIndex = 0;
+            canNum3.TabIndex = 0;
+            canNum4.TabIndex = 0;
+        }
+
+        private void Submit_Click(object sender, EventArgs e)
+        {
+
+            if (boxSummaryA < boxSummaryD) {
+
+                biastResults = boxSummaryA;
+                boxSummaryA = boxSummaryD;
+                boxSummaryD = biastResults;
+
+            }
+
+            resultSort[0] = boxSummaryA;
+            resultSort[1] = boxSummaryB;
+            resultSort[2] = boxSummaryC;
+            resultSort[3] = boxSummaryD;
+
+            Array.Sort(resultSort);
+            Array.Reverse(resultSort);
+
+            canAPlacement = Array.IndexOf(resultSort, boxSummaryA);
+            canBPlacement = Array.IndexOf(resultSort, boxSummaryB);
+            canCPlacement = Array.IndexOf(resultSort, boxSummaryC);
+            canDPlacement = Array.IndexOf(resultSort, boxSummaryD);
+
+            for (int i = 0; i < resultSort.Length; i++)
+            {
+
+                if (i == canAPlacement)
+                {
+
+                    if (i == 0)
+                    {
+                        resultTXT1.Text = (canNameA + " - " + resultSort[i]);
+                    }
+                    else if (i == 1)
+                    {
+                        resultTXT2.Text = (canNameA + " - " + resultSort[i]);
+                    }
+                    else if (i == 2)
+                    {
+                        resultTXT3.Text = (canNameA + " - " + resultSort[i]);
+                    }
+                    else if (i == 3)
+                    {
+                        resultTXT4.Text = (canNameA + " - " + resultSort[i]);
+                    }
+
+                }
+                else if (i == canBPlacement)
+                {
+
+                    if (i == 0)
+                    {
+                        resultTXT1.Text = (canNameB + " - " + resultSort[i]);
+                    }
+                    else if (i == 1)
+                    {
+                        resultTXT2.Text = (canNameB + " - " + resultSort[i]);
+                    }
+                    else if (i == 2)
+                    {
+                        resultTXT3.Text = (canNameB + " - " + resultSort[i]);
+                    }
+                    else if (i == 3)
+                    {
+                        resultTXT4.Text = (canNameB + " - " + resultSort[i]);
+                    }
+
+                }
+                else if (i == canCPlacement)
+                {
+
+                    if (i == 0)
+                    {
+                        resultTXT1.Text = (canNameC + " - " + resultSort[i]);
+                    }
+                    else if (i == 1)
+                    {
+                        resultTXT2.Text = (canNameC + " - " + resultSort[i]);
+                    }
+                    else if (i == 2)
+                    {
+                        resultTXT3.Text = (canNameC + " - " + resultSort[i]);
+                    }
+                    else if (i == 3)
+                    {
+                        resultTXT4.Text = (canNameC + " - " + resultSort[i]);
+                    }
+
+                }
+                else if (i == canDPlacement)
+                {
+
+                    if (i == 0)
+                    {
+                        resultTXT1.Text = (canNameD + " - " + resultSort[i]);
+                    }
+                    else if (i == 1)
+                    {
+                        resultTXT2.Text = (canNameD + " - " + resultSort[i]);
+                    }
+                    else if (i == 2)
+                    {
+                        resultTXT3.Text = (canNameD + " - " + resultSort[i]);
+                    }
+                    else if (i == 3)
+                    {
+                        resultTXT4.Text = (canNameD + " - " + resultSort[i]);
+                    }
+
+
+                }
+
+            }
+        }
+    }
+}
